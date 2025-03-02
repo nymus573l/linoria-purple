28c1ee698ffbf2b9cab3384a0f4f76355c89c2ba
Anonymous_573l <137441255+nymus573l@users.noreply.github.com>
2025-03-02 21:06:23 +0100

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -32,8 +32,8 @@ local Library = {
     FontColor = Color3.fromRGB(255, 255, 255);
     MainColor = Color3.fromRGB(28, 28, 28);
     BackgroundColor = Color3.fromRGB(20, 20, 20);
-    AccentColor = Color3.fromRGB(147, 112, 219);
-    OutlineColor = Color3.fromRGB(138, 43, 226);
+    AccentColor = Color3.fromRGB(0, 85, 255);
+    OutlineColor = Color3.fromRGB(50, 50, 50);
     RiskColor = Color3.fromRGB(255, 50, 50),
 
     Black = Color3.new(0, 0, 0);
@@ -45,6 +45,7 @@ local Library = {
     Signals = {};
     ScreenGui = ScreenGui;
 };
+
 local RainbowStep = 0
 local Hue = 0
 
