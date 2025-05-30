21d0a219eb3f39a446ce79989b6ee4900c13f0f1
Anonymous_573l <137441255+nymus573l@users.noreply.github.com>
2025-05-30 19:18:42 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,6 @@
 
 if game.PlaceId == 2788229376 then
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3988469a80ca54b62d65824a836cbd24.lua"))()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/d6e2232f857148d13cbc25a592abf9c5.lua"))()
 else
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/6347f627a845c85163f3f6bfe8d4c44a.lua"))()
 end
